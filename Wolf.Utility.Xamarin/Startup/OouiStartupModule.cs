acd84a0761129e71ed5645a1881f35e1a8f6be06
André Steenhoff Madsen <andre@steenhoff.dk>
2020-03-24 15:24:25 +0100

    end of day
    created a startup module for Ooui called OouiStartupModule

---
+++ Wolf.Utility.Xamarin/Startup/OouiStartupModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Wolf.Utility.Main.Startup;
+
+namespace Wolf.Utility.Xamarin.Startup
+{
+    public class OouiStartupModule : IStartupModule
+    {
+        public SetupXamarinDelegate SetupXamarin { get; }
+        public delegate void SetupXamarinDelegate();
+
+        public OouiStartupModule(SetupXamarinDelegate setup)
+        {
+            SetupXamarin = setup;
+        }
+
+        public void SetupServices(IServiceCollection services)
+        {
+            
+        }
+
+        public void ConfigureApplication(IApplicationBuilder app)
+        {
+            app.UseOoui();
+
+            SetupXamarin?.Invoke();
+        }
+    }
+}
